04e1af51475c573f77404fa890e01797005af62a
jlcastro <joselucianocastro10@gmail.com>
2025-11-15 15:34:30 +0100

    07: prefetch categories

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -1,49 +1,59 @@
-"use client";
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import React, {useState, useEffect} from "react";
+import { ReactQueryProvider } from "../react-query-provider";
+import { getQueryClient } from "../query-client";
 
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
-import { SearchFilters } from "./search-filters";
+import { SearchFilters, SearchFiltersSkeleton } from "./search-filters";
 
 interface Props {
     children: React.ReactNode
 }
 
-const Layout = ({children}: Props) => {
-    const [categories, setCategories] = useState([])
-
-    useEffect(() => {
-        const categories = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/`, {
-                    method: "GET",
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-                if (!response.ok) {
-                    //Manejo de errores si el backend devuelve un estado no existoso
-                    const errorData = await response.json();
-                    console.error("Error en el backend", errorData)
-                return;
-                }
-                //si la respuesta es exitosa
-                const data = await response.json()
-            
-                setCategories(data)
-            } catch (error) {
-                    //Manejo de errores de red o conexion
-                    console.error("Error al enviar datos:", error)
-            }
+const fetchCategories = async () => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/`, {
+            method: "GET",
+            cache: "no-store", // evita que nextJs use su propio cache
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            //Manejo de errores si el backend devuelve un estado no existoso
+            const errorData = await response.json();
+            console.error("Error en el backend", errorData)
+            return;
         }
-        categories();
-    }, [])
+        //si la respuesta es exitosa
+        return response.json()
+
+    } catch (error) {
+        //Manejo de errores de red o conexion
+        console.error("Error al enviar datos:", error)
+    }
+}
+
+const Layout = async ({children}: Props) => {
+    const queryClient = getQueryClient()
+
+    //prefetch de categorías
+    await queryClient.prefetchQuery({queryKey: ["categories"], queryFn: fetchCategories})
 
     return(
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <SearchFilters data={categories}/>
+
+            <ReactQueryProvider>
+                <HydrationBoundary state={dehydrate(queryClient)}>
+                    <Suspense fallback={<SearchFiltersSkeleton />}>
+                        <SearchFilters/>
+                    </Suspense>
+                </HydrationBoundary>
+            </ReactQueryProvider>
+            
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
--- frontend/src/app/(home)/search-filters/index.tsx
+++ frontend/src/app/(home)/search-filters/index.tsx
@@ -1,17 +1,59 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
-import { CustomCategory } from "../types";
 
-interface Props {
-    data: CustomCategory[];
+
+const fetchCategories = async () => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            //Manejo de errores si el backend devuelve un estado no existoso
+            const errorData = await response.json();
+            console.error("Error en el backend", errorData)
+            return;
+        }
+        //si la respuesta es exitosa
+        return response.json()
+
+    } catch (error) {
+        //Manejo de errores de red o conexion
+        console.error("Error al enviar datos:", error)
+    }
 }
 
-export const SearchFilters = ({data}: Props) => {
+export const SearchFilters = () => {
+    const {data} = useSuspenseQuery({queryKey: ['categories'], queryFn: fetchCategories,})
+
     return(
-        <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-            <SearchInput data={data}/>
+        <div
+            style={{backgroundColor: "#F5F5F5"}} 
+            className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
+        >
+            <SearchInput />
             <div className="hidden lg:block">
                 <Categories data={data}/>
+            </div>
+        </div>
+    )
+}
+
+export const SearchFiltersSkeleton = () => {
+    return(
+        <div 
+            className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
+            style={{backgroundColor: "#F5F5F5"}}
+        >
+            <SearchInput disabled/>
+            <div className="hidden lg:block">
+                <div className="h-11"/>
             </div>
         </div>
     )
